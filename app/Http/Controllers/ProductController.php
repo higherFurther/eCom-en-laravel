131e474576da76cc35dbc3d3844769b352589052
Armand <mwingum01@gmail.com>
2022-07-18 17:41:17 +0000

    Show My Order List

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 use Session;
 use Illuminate\Support\Facades\DB;
 
@@ -67,5 +68,47 @@ class ProductController extends Controller
     function removeCart($id){
         Cart::destroy($id);
         return redirect('cartList');
+    }
+
+    function orderNow(){
+        $userId = Session()->get('user')['id'];
+        $total = DB::table('cart')
+        ->join('products', 'cart.product_id','=','products.id')
+        ->where('cart.user_id',$userId)
+        ->select('products.*','cart.id as cart_id')
+        ->sum('products.price');
+
+        return view('ordernow',['total'=>$total]);
+    }
+
+    function orderPlace(Request $req){
+        
+        $userId = Session()->get('user')['id'];
+        $allCart = Cart::where('user_id', $userId)->get();
+        foreach($allCart as $cart){
+            $order = new Order;
+            $order->product_id = $cart['product_id'];
+            $order->user_id = $cart['user_id'];
+            $order->status = "Pending";
+            $order->payment_method = $req->payment;
+            $order->payment_status = "Pending";
+            $order->address = $req->address;
+            $order->save();
+            $allCart = Cart::where('user_id', $userId)->delete();
+        }
+
+        $req->input();
+
+        return redirect('/');
+    }
+
+    function myOrders(){
+        $userId = Session()->get('user')['id'];
+        $orders = DB::table('orders')
+        ->join('products', 'orders.product_id','=','products.id')
+        ->where('orders.user_id',$userId)
+        ->get();
+
+        return view('myorders',['orders'=>$orders]);
     }
 }
